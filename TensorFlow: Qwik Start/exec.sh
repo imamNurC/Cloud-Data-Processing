47e97c7ae12da2b0b609a7e2e25e8273fd4b9d57
Mr.Ackerman <imamnurcakra@gmail.com>
2024-06-13 21:12:28 +0700

    TFcloud

---
+++ TensorFlow: Qwik Start/exec.sh
@@ -0,0 +1,20 @@
+# Open Terminal 
+
+pip install google-cloud-logging
+pip install ---upgrade protobuf
+pip install --upgrade tensorflow
+python --version
+
+
+# check version
+python -c "import tensorflow;print(tensorflow.__version__)"
+
+touch model.py
+
+echo "
+
+" >> model.py
+
+
+
+
